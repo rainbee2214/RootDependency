b41351f51e85a88fa744cc294f7cefda9a281e33
rainbee2214 <rainbee2214@gmail.com>
2015-01-10 00:49:53 -0400

    Working on generator script.

--- RootDependency/Assets/Scripts/PrimeGenerator.cs
+++ RootDependency/Assets/Scripts/PrimeGenerator.cs
@@ -3,28 +3,61 @@ using System.Collections;
 
 public class PrimeGenerator : MonoBehaviour
 {
-    public int upperBound = 10;
+    public int upperBoundNumbers = 10;
     GameObject[] numbers;
-    
+
+    int lowerBound = 0;
+    int upperBound = 2;
+    int count = 1;
+    int direction = 1;
+
+    int negation = -1;
+
     void Awake()
     {
-        numbers = new GameObject[upperBound];
+        numbers = new GameObject[upperBoundNumbers];
         GameObject numberPrefab = Resources.Load("Prefabs/Number", typeof(GameObject)) as GameObject;
-        for (int i = 0; i < upperBound; i++)
+        for (int i = 0; i < upperBoundNumbers; i++)
         {
             numbers[i] = Instantiate(numberPrefab) as GameObject;
             numbers[i].name = ""+i;
             numbers[i].transform.parent = transform;
         }
+        //Place quads in position to create spiral
+        Vector2 position = Vector2.zero;
+        int k = 0;
+        foreach (GameObject number in numbers)
+        {
+            //Determine movement amount based on i
+            position += GetMovement(count);
+            number.transform.position = position;
+            k++;
+        }
     }
 
-    void Start()
+    Vector2 GetMovement(int i)
     {
+        Debug.Log("Getting movement! " + i);
+        Vector2 m = Vector2.right;
 
+        if (i >= lowerBound && i < upperBound)
+        {
+            if (i == upperBound) Increment(i);
+
+            //Find whether its positive or negative
+            //Find out how many iterations we need
+
+            count++;
+        }
+
+        return m;
     }
 
-    void Update()
+    void Increment(int i)
     {
-
+        Debug.Log("Increment! " + i);
+        //Increments lowerBound, upperBound
+        lowerBound = upperBound;
+        upperBound += count * 2;
     }
 }
