a80709d69cb04517a78c6b401619bce499066d3f
rainbee2214 <rainbee2214@gmail.com>
2015-01-09 23:54:21 -0400

    Has been restructured for the generation of small primes ;)

---
+++ RootDependency/Assets/Scripts/PrimeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrimeGenerator : MonoBehaviour
+{
+    public int upperBound = 10;
+    GameObject[] numbers;
+    
+    void Awake()
+    {
+        numbers = new GameObject[upperBound];
+        GameObject numberPrefab = Resources.Load("Prefabs/Number", typeof(GameObject)) as GameObject;
+        for (int i = 0; i < upperBound; i++)
+        {
+            numbers[i] = Instantiate(numberPrefab) as GameObject;
+            numbers[i].name = ""+i;
+            numbers[i].transform.parent = transform;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
