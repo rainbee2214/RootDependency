9c2ba2995b3250b4f9706b5efc8ce071f1c67631
rainbee2214 <rainbee2214@gmail.com>
2015-07-02 17:30:46 -0300

    Added a spiral. Easy as pie ;)

---
+++ RootDependency/Assets/Scripts/Spiral.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spiral : MonoBehaviour
+{
+    [Range(0f,100f)]        public float radius = 3f;
+    [Range(0f, Mathf.PI*2)] public float theta = 0f;
+    [Range(0f, 1f)]         public float delta = 0.1f;
+    public bool reversed;
+    
+    Vector2 position;
+
+    void Awake()
+    {
+        StartCoroutine("StartSpiral");
+    }
+
+    void GetPolar()
+    {
+        position.Set(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta));
+        transform.position = position;
+        theta = reversed ? theta < 0 ? Mathf.PI*2 : theta - delta : theta > Mathf.PI * 2 ? 0 : theta + delta;
+    }
+
+    IEnumerator StartSpiral()
+    {
+        while (true)
+        {
+            GetPolar();
+            yield return null;
+        }
+    }
+}
